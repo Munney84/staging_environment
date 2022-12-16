40e5a273bc9427b151476e6befae5e214f677a5f
Kevin Sanaycela <kevinsanaycela@Kevins-MacBook-Air.local>
2022-12-16 01:12:05 -0500

    main

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,16 +25,6 @@ output "instance_ip" {
   
 }
 
-
-# VPC
-resource "aws_vpc" "my-vpc" {
-  cidr_block           = "10.0.0.0/16" 
-  enable_dns_hostnames = "true"
- 
-  tags = {
-    "Name" : "ifme-staging-vpc"
-  }
-}
  
  
 # SUBNET 1 (PUBLIC)
