da85a200821d6da509a1253bef3768f8898b961a
Kevin Sanaycela <kevinsanaycela@Kevins-MacBook-Air.local>
2022-12-16 03:41:02 -0500

    test

--- terraform/install_docker.sh
+++ terraform/install_docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#TESTING 
+
 #Clone Application Repo Onto Newly Created EC2 and Cronjob
 export HOME=/home/ubuntu
 sudo -- bash -c 'echo "* * * * * ubuntu cd staging_environment/ && git pull origin main && sleep 10 && git pull origin main && sleep 10 && git pull origin main && sleep 10 && git pull origin main && sleep 10 && git pull origin main && sleep 10 && git pull origin main" >> /etc/crontab'
